a916680bffb9587ac0bd23b71d7d5af67e3fb8e6
user <user@DESKTOP-AF7P9D7>
2023-05-31 00:42:12 -0700

    Add project files.

---
+++ SamianSoft.Domain/Entity/Field.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SamianSoft.Domain.Entity
+{
+    public class Field
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ SamianSoft.Domain/Entity/ObjectTemplate.cs
@@ -0,0 +1,17 @@
+﻿namespace SamianSoft.Domain.Entity
+{
+    public class ObjectTemplate:Field
+    {
+        public int CreateUserId { get; set; }
+        public string RequestUrl { get; set; }
+        public string SessionId { get; set; }
+        public string ClientIP { get; set; }
+        public string RequestId { get; set; }
+        public string HttpMethod { get; set; }
+        public string Host { get; set; }
+        public string Agent { get; set; }
+        public string XClientVersion { get; set; }
+        public string XClientName { get; set; }
+        public string ApiVersion { get; set; }
+    }
+}
