feddb01c5158410b40bd98d0bfdff543d04ffd5e
SamianSoftSystem <user@DESKTOP-***>
2023-05-31 02:02:59 -0700

    Create dbcontext and Idbcontext for handling the dbsets

---
+++ Persistence/Data/SF_DbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamianSoft.Domain.DataInterface;
+using SamianSoft.Domain.Entity;
+
+namespace SamianSoft.Persistence.Data
+{
+    public class SFDbContext : DbContext, ISF_DbContext
+    {
+        #region Consturcotr
+        public SFDbContext(DbContextOptions<SFDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        #endregion
+
+        #region Impeliment Entities
+        public DbSet<ObjectTemplate> ObjectTemplates { get; set; }
+        #endregion
+
+        #region Methods
+        public async Task<int> SaveChangesAsync() => await base.SaveChangesAsync();
+        #endregion
+
+        #region OvverRides
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        #endregion
+    }
+}
---
+++ SamianSoft.Domain/DataInterface/ISF_DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamianSoft.Domain.Entity;
+
+namespace SamianSoft.Domain.DataInterface
+{
+    public interface ISF_DbContext:IDisposable
+    {
+        DbSet<ObjectTemplate> ObjectTemplates { get; set; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
