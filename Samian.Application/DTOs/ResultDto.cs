33d743b302e520e155898dacf562766108039077
SamianSoftSystem <user@DESKTOP-***>
2023-05-31 02:03:18 -0700

    create a result dto to handle a results from the services

---
+++ Samian.Application/DTOs/ResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SamianSoft.Application.DTOs
+{
+    public class ResultDto
+    {
+        public object? Data { get; set; }
+        public bool IsSuccess { get; set; } = false;
+        public int StatusCode { get; set; } = 400;
+        public string? Message { get; set; }
+    }
+}
