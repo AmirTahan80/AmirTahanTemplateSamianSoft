c730c30595f3b550d8b2d3020e0dc17dcbb0e9f1
SamianSoftSystem <user@DESKTOP-***>
2023-06-01 00:40:27 -0700

    clean Program.cs file

--- AmirTahanTemplateSamianSoft/Program.cs
+++ AmirTahanTemplateSamianSoft/Program.cs
@@ -1,4 +1,3 @@
-
 using SamianSoft.Application.Services.ObjectTemplate.Commands;
 using SamianSoft.Persistence.Data;
 
@@ -9,34 +8,24 @@ namespace AmirTahanTemplateSamianSoft
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
             builder.Services.AddAutoMapper(typeof(Program));
             builder.Services.AddControllers();
+            #region AddDbContext
             builder.Services.AddDbContext<SFDbContext>();
-
+            #endregion
             #region Injections
             builder.Services.AddScoped<IAddObjectTemplateRepository, AddObjectTemplateRepository>();
             #endregion
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
