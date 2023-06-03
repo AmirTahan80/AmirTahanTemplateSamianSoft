44d6a4305f8e9e97f9fa0edb64469fef9917eb78
SamianSoftSystem <user@DESKTOP-***>
2023-06-02 22:21:41 -0700

    add required attributes for the entity.

--- Samian.Application/Services/ObjectTemplate/ObjectTemplateDto.cs
+++ Samian.Application/Services/ObjectTemplate/ObjectTemplateDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace SamianSoft.Application.Services.ObjectTemplate
 {
-    public record class ObjectTemplateDto(int CreateUserId,string RequestUrl, string SessionId, string ClientIP,
+    public record class ObjectTemplateDto(int CreateUserId, string RequestUrl, string SessionId, string ClientIP,
         string RequestId, string HttpMethod, string Host, string Agent, string XClientVersion,
         string XClientName, string ApiVersion);
 }
--- SamianSoft.Domain/Entity/ObjectTemplate.cs
+++ SamianSoft.Domain/Entity/ObjectTemplate.cs
@@ -1,17 +1,30 @@
-﻿namespace SamianSoft.Domain.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SamianSoft.Domain.Entity
 {
     public class ObjectTemplate:Field
     {
+        [Required]
         public int CreateUserId { get; set; }
-        public string RequestUrl { get; set; }
-        public string SessionId { get; set; }
-        public string ClientIP { get; set; }
-        public string RequestId { get; set; }
-        public string HttpMethod { get; set; }
-        public string Host { get; set; }
-        public string Agent { get; set; }
-        public string XClientVersion { get; set; }
-        public string XClientName { get; set; }
+        [Required]
+        public string RequestUrl { get; set; }                          
+        [Required]
+        public string SessionId { get; set; }                           
+        [Required]
+        public string ClientIP { get; set; }                            
+        [Required]
+        public string RequestId { get; set; }                           
+        [Required]
+        public string HttpMethod { get; set; }                          
+        [Required]
+        public string Host { get; set; }                                
+        [Required]
+        public string Agent { get; set; }                               
+        [Required]
+        public string XClientVersion { get; set; }                      
+        [Required]
+        public string XClientName { get; set; }                         
+        [Required]
         public string ApiVersion { get; set; }
     }
 }
